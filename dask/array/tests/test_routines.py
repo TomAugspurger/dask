0ce4154b73a5686a046ec413f8ab557f272fb088
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-08-19 10:53:15 -0500

    skip for numpy 1.14

--- dask/array/tests/test_routines.py
+++ dask/array/tests/test_routines.py
@@ -12,6 +12,7 @@ import dask.array as da
 from dask.compatibility import PY2
 from dask.utils import ignoring
 from dask.array.utils import assert_eq, same_keys, AxisError
+from dask.array.numpy_compat import _numpy_115
 
 
 def test_array():
@@ -793,6 +794,9 @@ def test_roll(chunks, shift, axis):
 )
 @pytest.mark.parametrize("reverse", [True, False])
 def test_union1d(shape, reverse):
+    if any(len(x) > 1 for x in shape) and not _numpy_115:
+        pytest.skip(reason="NumPy-10563.")
+
     s1, s2 = shape
     x1 = np.arange(12).reshape(s1)
     x2 = np.arange(6, 18).reshape(s2)
